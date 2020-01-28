57d9b9c8a5761c425283155f5f2af6bdf5ad527b
lucasluz99 <lucasluzthatsme@gmail.com>
2020-01-28 15:43:28 -0300

    Add lib unform

--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,18 +1,23 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Form, Input } from '@rocketseat/unform';
+
 import logo from '../../assets/logo.svg';
 
 export default function SignUp() {
+  function handleSubmit(data) {
+    console.tron.log(data);
+  }
   return (
     <>
       <img src={logo} alt="Logo GoBarber" />
-      <form>
-        <input type="text" placeholder="Seu nome completo" />
-        <input type="email" placeholder="Seu e-mail" />
-        <input type="password" placeholder="Sua senha" />
+      <Form onSubmit={handleSubmit}>
+        <Input name="name" type="text" placeholder="Seu nome completo" />
+        <Input name="email" type="email" placeholder="Seu e-mail" />
+        <Input name="password" type="password" placeholder="Sua senha" />
         <button type="submit">Criar conta</button>
         <Link to="/">Já tenho login</Link>
-      </form>
+      </Form>
     </>
   );
 }
